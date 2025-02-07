dcd984fe9d77090608fa9e74a6a11a2d8c26acd3
오청열 <ocy@nis.co.kr>
2025-02-07 17:57:01 +0900

    fix: scss 배포를 위한 정리

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,11 +1,13 @@
-import gulp from 'gulp';
-import fileInclude from 'gulp-file-include';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import sourcemaps from 'gulp-sourcemaps';
-import concat from 'gulp-concat';
-import autoprefixer from 'gulp-autoprefixer';
-import browserSyncPkg from 'browser-sync';
+import gulp from 'gulp'; // Gulp: 작업 자동화 도구
+import fileInclude from 'gulp-file-include'; // gulp-file-include: HTML 파일 포함 플러그인
+import dartSass from 'sass'; // sass: Dart Sass 컴파일러
+import gulpSass from 'gulp-sass'; // gulp-sass: Gulp용 Sass 컴파일러
+import sourcemaps from 'gulp-sourcemaps'; // gulp-sourcemaps: 소스 맵 생성 플러그인
+import concat from 'gulp-concat'; // gulp-concat: 파일 병합 플러그인
+import autoprefixer from 'gulp-autoprefixer'; // gulp-autoprefixer: CSS 자동 접두사 추가 플러그인
+import browserSyncPkg from 'browser-sync'; // browser-sync: 브라우저 동기화 및 라이브 리로드 도구
+import { deleteAsync } from 'del'; // del: 파일 및 디렉토리 삭제 도구
+
 
 const browserSync = browserSyncPkg.create();
 const sass = gulpSass(dartSass);
@@ -39,6 +41,11 @@ export const js = () => {
     .pipe(gulp.dest('./dist/resources/js'));
 };
 
+// Clean Dist Folder (강제 삭제 옵션 추가)
+export const clean = () => {
+  return deleteAsync(['./dist'], { force: true });
+};
+
 // Serve with Live Reload
 export const serve = () => {
   browserSync.init({
@@ -50,5 +57,6 @@ export const serve = () => {
   gulp.watch('./src/js/**/*.js', gulp.series(js)).on('change', browserSync.reload);
 };
 
+
 // Default Task
-export default gulp.series(html, scss, js, serve);
+export default gulp.series(clean, gulp.parallel(html, scss, js), serve); // clean을 먼저 실행하고, 나머지는 병렬로 실행한 후 serve 실행
